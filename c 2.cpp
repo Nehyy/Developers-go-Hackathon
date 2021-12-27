1b29d2f02cd86d84d37cb143494a13e9ed0b6108
Dornala Neha <96726485+Nehyy@users.noreply.github.com>
2021-12-27 19:32:18 +0530

    Add files via upload

---
+++ c 2.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int x=10;
+		if (x>5)
+	{
+		printf("x is greater than 5");
+	}
+	else if(x<8)
+	{
+		printf("x is less than 8");
+	}
+	else if(x==10)
+	{
+		printf("x is equal to 10");
+	}
+	else
+	{
+		printf("no condiion is true");
+	}
+} 
